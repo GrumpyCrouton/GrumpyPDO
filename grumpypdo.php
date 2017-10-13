417a17d7af314bea38ba215533a0ec59aa681fde
GrumpyCrouton <grumpythe1st@gmail.com>
2017-10-13 12:29:12 -0500

    Overhaul of some code.
    I have overhauled a lot of code based on the comments and advice given to me on StackOverflow.

--- grumpypdo.php
+++ grumpypdo.php
@@ -1,64 +1,58 @@
 <?php
-	class GrumpyPdo {
-		
-		private $host, $user, $pass, $db, $charset;
-		private $dsn, $databaseObject;
-		
-		private $killOnError = true;
-		private $echoOnError = false;
-		
-		//these options can obviously be changed but not "on the fly" (yet).
-		private $opt = [
-			PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-			PDO::ATTR_EMULATE_PREPARES   => false,
-		];
-		
-		function __construct($host, $user, $pass, $db, $charset = "utf8") {
-			
-			$this->setData([
-				"host" => $host,
-				"user" => $user,
-				"pass" => $pass,
-				"db" => $db,
-				"charset" => $charset,
-			]);
-			
-			$this->createDBObject();
-			
-		}
-		
-		function query($query, $values = []) {
-			if(empty($values)) {
-				$stmt = $this->databaseObject->prepare($query);
+
+class GrumpyPdo extends \PDO
+{
+	/**
+     * @var bool
+     */
+    private $killOnError = false; //if true causes loss of stacktrace but will stop page load. Will still log error code.
+	private $echoOnError = false; //if true displays error message to user loading page
+    
+	/**
+     * @var array
+     */
+	private $opt = [
+		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+		PDO::ATTR_EMULATE_PREPARES => false,
+	];
+	
+	/**
+     * @var string
+     */
+	private $charset = "utf8";
+	
+    public function __construct($host, $user, $pass, $db, $opt = [])
+    {
+		$charset = empty($opt['charset'])? $this->charset : $opt['charset'];
+		$options = empty($opt['options'])? $this->opt : $opt['options'];
+		parent::__construct($this->getDSN($host, $db, $charset), $user, $pass, $options);
+    }
+
+    public function run($query, $values = [])
+    {
+		try {
+			if (!empty($values)) {
+				$stmt = $this->prepare($query);
 				$stmt->execute($values);
 			} else {
-				$stmt = $this->databaseObject->query($query);
+				$stmt = $this->query($query);
 			}
-			return $stmt;
+		} catch (PDOException $e) {
+			$this->handleError($e->getMessage());
+            throw  $e;
 		}
-		
-		function setData($data) {
-			if(!is_array($data)) {
-				$this->handleError("GrumpyPDO: Error Code SD13; The setData method requires an array with \$key => \$value pairs.");
-			}
-			foreach($data as $k => $v) {
-				$this->{$k} = $v;
-			}
-			return true;
-		}
-		
-		function createDBObject() {
-			$this->dsn = "mysql:host={$this->host};dbname={$this->db};charset={$this->charset}";
-			$this->databaseObject = new PDO($this->dsn, $this->user, $this->pass, $this->opt);
-		}
-		
-		function handleError($error) {
-			error_log($error);
-			if($this->echoOnError) echo $error;
-			if($this->killOnError) die($error);
-		}
-		
-		
+        return $stmt;
+    }
+
+	private function getDSN($host, $db, $charset) {
+		return "mysql:host={$host};dbname={$db};charset={$charset}";
 	}
-?>
+	
+    private function handleError($error)
+    {
+        error_log($error);
+        if ($this->echoOnError) echo $error;
+        if ($this->killOnError) die();
+    }
+}
