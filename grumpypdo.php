76f70ef6575099392126aabbd66b8aa082f33845
GrumpyCrouton <grumpythe1st@gmail.com>
2022-10-11 23:31:30 -0500

    Revert "Less Strict DSN and verified table caching"
    This reverts commit 3704561acd810555eeef20e49cafc7f492d17768.

--- grumpypdo.php
+++ grumpypdo.php
@@ -9,45 +9,21 @@ class GrumpyPdo extends \PDO
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false,
     );
-
-    private $verified_databases = [];
-
-    public function __construct($hostname, $username, $password, $database, $attributes = array(), $dsn_prefix = "mysql", $dsn_param_string = "")
+    public function __construct($hostname, $username, $password, $database, $attributes = array() , $charset = "utf8")
     {
-        parent::__construct(
-            "{$dsn_prefix}:host={$hostname};dbname={$database};{$dsn_param_string}", 
-            $username, 
-            $password, 
-            array_replace($this->default_attributes, $attributes)
-        );
+        $active_attrs = $this->default_attributes;
+        if (!empty($attributes))
+        {
+            array_replace($active_attrs, $attributes);
+        }
+        parent::__construct("mysql:host={$hostname};dbname={$database};charset={$charset}", $username, $password, $active_attrs);
     }
 
     /**
-     * @var string - A parameterized query string using either anonymous placeholders or named placeholders.
-     * @var array - A key value pair of values to pass to the query. Should reflect the placeholders placed in the query, including position when using anonymous placeholders.
-     */
-    public function run($query, $values = array())
-    {
-        if (!$values)
-        {
-            return $this->query($query);
-        }
-        if (is_array($values[0]))
-        {
-            return $this->multi($query, $values);
-        }
-        $stmt = $this->prepare($query);
-        $stmt->execute($values);
-        return $stmt;
-    }
-
-    /**
-     * It takes a table name and an array of data to insert into the table
-     * 
-     * @param table The table to insert into
-     * @param inserts Array of key => value pairs
-     * 
-     * @return The return value is the result of the query.
+     * @var string - A table name, which must already exist in your database.
+     * @var array - A key value pair of values to insert into the database on the specified table
+     *
+     * Query will be generated with named placeholders.
      */
     public function insert($table, $inserts)
     {
@@ -76,8 +52,8 @@ class GrumpyPdo extends \PDO
 
     /**
      * @var string - A table name, which must already exist in your database.
-     * @var array - An array of key => value pairs used to generate the SET clause.
-     * @var array - An array of key => value pairs to generate the WHERE clause. Each set will be separated by ' AND '.
+     * @var array - A key value pair of values to pass to the queries SET clause.
+     * @var array - A key value pair of values to pass to the queries WHERE clause. Each set will be separated by 'AND'.
      */
     public function update($table, $updates, $where)
     {
@@ -90,96 +66,96 @@ class GrumpyPdo extends \PDO
             throw new Exception('One or more of the supplied columns do not exist in the supplied table');
         }
 
-        $data_sets = [
-            'set' => [
-                'array' => $updates, 'separator' => ', '
-            ],
-            'where' => [
-                'array' => $where, 'separator' => ' AND '
-            ],
-        ];
+        $query_params = array_merge(array_values($updates), array_values($where));
+        $set_clause_parts = [];
+        $where_clause_parts = [];
 
-        /* Creating a query string for the SET and WHERE clauses. */
-        foreach($data_sets as $type => &$d) {
-            foreach(array_keys($d['array']) as $key) {
-                print_r($key);
-                $d['query_clause'][] = "`{$key}`=?";
-            }
-            $d['query_clause'] = implode($d['separator'], $d['query_clause']);
+        foreach(array_keys($updates) as $key) {
+            $set_clause_parts[] = "`{$key}`=?";
+        }
+        foreach(array_keys($where) as $key) {
+            $where_clause_parts[] = "`{$key}`=?";
         }
 
-        return $this->run(
-            "UPDATE {$table} SET {$data_sets['set']['query_clause']} WHERE {$data_sets['where']['query_clause']}", 
-            array_merge(array_values($updates), array_values($where))
-        );
-    }
+        $set_clause = implode(', ', $set_clause_parts);
+        $where_clause = implode(' AND ', $where_clause_parts);
 
+        return $this->run("UPDATE {$table} SET {$set_clause} WHERE {$where_clause}", $query_params);
+    }
+    /**
+     * @var string - A parameterized query string using either anonymous placeholders or named placeholders.
+     * @var array - A key value pair of values to pass to the query. Should reflect the placeholders placed in the query, including position when using anonymous placeholders.
+     */
+    public function run($query, $values = array())
+    {
+        if (!$values)
+        {
+            return $this->query($query);
+        }
+        if (is_array($values[0]))
+        {
+            return $this->multi($query, $values);
+        }
+        $stmt = $this->prepare($query);
+        $stmt->execute($values);
+        return $stmt;
+    }
     /**
      * Quick queries
      * Allows you to run a query without chaining the return type manually. This allows for slightly shorter syntax.
      */
-    /**
-     * @return The first row of the result set.
+
+     /**
+     * Fetch a singular row from the database in a flat array.
      */
     public function row($query, $values = array())
     {
         return $this->run($query, $values)->fetch();
     }
     /**
-     * @return The first column of the first row of the result set.
-     */
+    * Fetch a single cell from the database. Doesn't support multiple rows.
+    */
     public function cell($query, $values = array())
     {
         return $this->run($query, $values)->fetchColumn();
     }
     /**
-     * @return An array of all the rows in the result set.
-     */
+    * Fetch all of the results from the database into a multidimensional array.
+    */
     public function all($query, $values = array())
     {
         return $this->run($query, $values)->fetchAll();
     }
-
     /**
-     * @return An array of values from a single column in the result set.
-     */
+    * Fetch a single column from the database. Similar to $this->cell, except can have multiple rows.
+    */
     public function column($query, $values = array())
     {
         return $this->run($query, $values)->fetchAll(\PDO::FETCH_COLUMN);
     }
 
     /**
-     * If the table is not in the verified_databases array, then check if it exists in the database. If
-     * it does, then add it to the verified_databases array.
-     * 
-     * @param table The table name
+     * Private Methods
+     * Methods called internally, and should not be used directly.
+     */
+
+     /**
+     * Used to verify if a table exists in the currently selected database or not.
      */
     private function verify_table($table)
     {
-        $exists = array_key_exists($table, $this->verified_databases);
-        if(!$exists && $exists = in_array($table, $this->column('SHOW TABLES'))) {
-            $this->verified_databases[$table] = $this->column("SHOW COLUMNS FROM `{$table}`");
-        }
-        return $exists;
+        return in_array($table, $this->column("SHOW TABLES"));
     }
-
     /**
-     * If the table exists in the verified_databases array, and the columns are all in the table, then
-     * return true
-     * 
-     * @param table The table name
-     * @param columns The columns you want to select from the table.
-     */
+    * Used to determine if columns exist in a certain table in the selected database or not.
+    */
     private function verify_columns($table, $columns)
     {
-        return array_key_exists($table, $this->verified_databases) && !array_diff($columns, $this->verified_databases[$table]);
+        $db_columns = $this->column("SHOW COLUMNS FROM `{$table}`");
+        return !array_diff($columns, $db_columns);
     }
-
     /**
     * Used to handle running queries with multiple data sets. Automatically used when passing an array of data sets to $this->run
-    * 
-    * @param query The query to execute
-    * @param values The data sets associated with the query, an array of key => value pairs
     */
     private function multi($query, $values = array())
     {
